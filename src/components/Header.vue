36e51d99c714cb989fe75dee1835d0adf7f6a20f
morganwang010 <morganwang010@sina.com>
2024-01-31 23:56:57 +0800

    add layouts

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">
+    <span>
+      <img
+        style="width: 50px; height: 50px; margin-bottom: -15px"
+        src="/imgs/logo.jpg" /></span
+    ><span>国家重点研发“合成生物学”专项：微生物天然产物的智能创建与改良</span>
+  </div>
+  <div class="title">
+    <span class="nav">
+      <a href="/">首页 &nbsp;</a> > 数据库-2018YFA0901903-菌种库</span
+    >
+  </div>
+</template>
